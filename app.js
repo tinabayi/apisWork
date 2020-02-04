82f5b9556709285428ef1c292044b11200b51a8c
tinabayi <bayizerechristine4@gmail.com>
2020-02-04 18:05:37 +0200

    Make a form to add product in sql database

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
-const cors = require('cors');
-app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('./public'));
+
 const mysql=require('mysql')
-const Connecction=mysql.createConnection({
+const Connection=mysql.createConnection({
    host:'localhost',
    user:'root',
    password:'',
    database:'product'
 
 })
-Connecction.connect((err)=>{
-    if(!err){
-        console.log("connect to product database")
+ 
+app.listen(8000,()=>{console.log('server running on port 8000')})
+
+Connection.connect((error)=>{
+    if(!error){
+          console.log("connect to product database")
+     }
+  })
 //GET API
         app.get('/apiswork/api/products', (req, res) => {
-        Connecction.query('SELECT * FROM products',(err,rows,field)=>{
+        Connection.query('SELECT * FROM products',(err,rows,field)=>{
         res.send(rows)
             })  
         });
-//POST API        
-       app.post('/apiswork/api/items',(req,res)=>{
-        let item= req.body;
-          if(item){
-          Connection.query('INSERT INTO product SET prodId = ?,prodName = ?, prodAmount = ?,',[item.prodId,item.prodName,item.prodAmount],function(err,rows,fields){
-                if(!err){
-                    res.send('successfull send')
-                }else{
-                    console.log(err)
-                    res.send(' data not send')
-          
-        }
-       
-       })
     
-
-
-const port = 3000;
-app.listen(port, () => {console.log(` app listening on port ${port}!`)})
+//POST API        
+     app.post('/apiswork/api/items',(req,res)=>{
+         let item= req.body;
+       console.log(item.prodId)
+        res.send( 'data sent')
+    const prodId=req.body.prodId
+    const prodName=req.body.prodName
+    const prodAmount=req.body.prodAmount
+     Connection.query('INSERT INTO products SET prodId = ?,prodName = ?, prodAmount = ?',[prodId,prodName,prodAmount],function(err,rows,fields){
+        if(!err){
+            res.send('successfull send')
+        }else{
+            //
+            console.log(err)
+            res.send(' data not send')
+        }
+    })     
+});
